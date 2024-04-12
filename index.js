8c5854eff928464a19df5b02eb9c6706676e6bde
Lugan2004 <visagielugan42@gmail.com>
2024-04-12 11:27:20 +0200

    Explain why the output is the same

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ const branchB = new BankBranch({ name: 'Prieska branch', address: '456 pappegaai
 // Use the getBranchInfo method to retrieve branch information
 console.log(branchA.getBranchInfo()); // Output:{ name: 'Cape Town branch', address: '123 Voortrekker Rd' }
 console.log(branchB.getBranchInfo()); // Output:{ name: 'Cape Town branch', address: '123 Voortrekker Rd' }
+//step by step explainantion on why the console will output the same values.
+// 1. The first time new BankBranch() is called with the branchA variable, the constructor checks if bankBranchInstance is null.
+// Since it is null, it creates a new instance of BankBranch with the provided branchInfo and assigns it to bankBranchInstance.
+// This new instance is then returned and assigned to the branchA variable.
+// 2. The second time new BankBranch() is called with the branchB variable, the constructor againchecks if bankBranchInstance is null.
+// Since it is not null, it returns the existing instance from the bankBranchInstance variable.
+// 3. The getBranchInfo() method is called on each instance and returns the branch information.
+// 4. The console.log() function's is used to print the branch information to the console.
+// 5. So the output will be the same for both instances.
 
 // Verify that branchA and branchB are referring to the same instance
 console.log(branchA === branchB); // Output: true
